9a9eae53344c517c6e13c8ae4066d5048253635f
Katie Little <kobirdie911@gmail.com>
2017-01-20 21:27:54 +0000

    update

---
+++ tones.lua
@@ -0,0 +1,23 @@
+ function love.update(dt)
+    if love.keyboard.isDown("q") then  
+        tone = love.audio.newSource("sound/tone1.wav", "static")  
+        tone: play()
+        end
+
+        if love.keyboard.isDown("w") then  
+        tone = love.audio.newSource("sound/tone2.wav", "static") 
+        tone: play()
+        end
+        if love.keyboard.isDown("e") then  
+            tone = love.audio.newSource("sound/tone3.wav", "static")  
+            tone: play()
+        end
+        if love.keyboard.isDown("a") then  
+            tone = love.audio.newSource("sound/tone4.wav", "static")  
+            tone: play()
+        end
+        if love.keyboard.isDown("s") then
+            tone = love.audio.newSource("sound/tone5.wav", "static")  
+            tone: play()
+        end
+    end
